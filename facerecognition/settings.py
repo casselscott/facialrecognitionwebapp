8b79db6237f0ad190698defce6f9e4d0331d5936
casselscott <casselscott.e@outlook.com>
2023-11-07 13:05:28 -0700

    updated files

--- facerecognition/settings.py
+++ facerecognition/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productiongit add README.md
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
