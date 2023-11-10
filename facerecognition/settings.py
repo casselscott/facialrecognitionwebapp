03dc3f555792109f4a319134a097b68d59256a1d
casselscott <casselscott.e@outlook.com>
2023-11-10 16:37:54 -0700

    updated requirements

--- facerecognition/settings.py
+++ facerecognition/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
    
 ]
 
@@ -131,7 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # media
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
